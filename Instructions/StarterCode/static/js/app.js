da4252b4943e3d6d68739a54fce920464960e426
Abbie Kent <arkent10@gmail.com>
2021-01-13 12:58:45 -0500

    updating

--- Instructions/StarterCode/static/js/app.js
+++ Instructions/StarterCode/static/js/app.js
@@ -1,22 +1,51 @@
 function getData(){
+    var selector = d3.select('#selDataset');
+
+    d3.json('data/samples.json').then((data)=>{
+        var ids =data.names;
+        ids.forEach((id)=>{
+            selector.append('option').text(id).property('value', id)
+        })
+    var firstid= ids[0];
+
+    createChart(firstid)
+    })
+};
+
+function createChart(subjectID){
     d3.json('data/samples.json').then((data)=>{
     var names= data.names;
     var metadata= data.metadata;
     var samples = data.samples;
-    // console.log(samples)
+    var findsubject = metadata.filter((d)=> d.id== subjectID);
+    var result = findsubject[0];
+      var metaPanel = d3.select("#sample-metadata");
+      console.log(result);
+    //   console.log(metaPanel);
+      metaPanel.html("");
+      Object.entries(result).forEach(([key, value]) => {
+        metaPanel.append("h6").text(`${key.toUpperCase()}: ${value}`)
+        // console.log(samples)
+    })
+
 //************************************************************************************** */
 //HORIZONTAL BART CHART
 //************************************************************************************** */
 
     //x axis value
-    var sampleValues = samples[0].sample_values.sort((a,b)=> b-a).slice(0,10).reverse();
+    var sample_values = result.sample_values
+    // .sort((a,b)=>b-a).slice(0,10).reverse();
+
+    // var sampleValues = samples[0].sample_values.sort((a,b)=> b-a).slice(0,10).reverse();
     // console.log(sampleValues);
     //y values 
+    console.log(result)
+    // var otuIDs = result.otuIDs.map((d)=> `OTU ${d}`).slice(0,10).reverse();
     var otuIDs= samples[0].otu_ids.map((d)=> `OTU ${d}`).slice(0,10).reverse();
     // console.log(outIDs)
     var otuLabels= samples[0].otu_labels.slice(0,10).reverse();
     var trace1= {
-        x: sampleValues,
+        x: sample_values,
         y: otuIDs,
         type: 'bar',
         orientation: 'h',
@@ -59,4 +88,9 @@ var layout={
 
 });
 };
+
+function optionChanged(newSample) {
+    // Fetch new data each time a new sample is selected
+    createChart(newSample)
+  }
 getData();
